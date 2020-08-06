f635d1b8216564a8e5beabf0b4b0d9a70f29593e
Sarfaraz Merchant <smerchant1@wsgc.com>
2020-08-05 23:46:10 -0700

    Test tile interaction

--- src/__tests__/Game.test.js
+++ src/__tests__/Game.test.js
@@ -15,5 +15,32 @@ describe("Game Unit Test", () => {
       const wrapper = mount(<Game />);
       expect(wrapper.find("div#board").length).toBe(1);
     });
+
+    it("should render 9 tiles", () => {
+      const wrapper = mount(<Game />);
+      expect(wrapper.find("button.tile").length).toBe(9);
+    });
+  });
+
+  describe("when clicked on tile", () => {
+    it("should set button text to 'X'", () => {
+      const wrapper = mount(<Game />);
+      const firstBtn = wrapper.find("button.tile").at(0);
+      firstBtn.simulate("click");
+      expect(firstBtn.text()).toBe("X");
+    });
+
+    it("should alternate between 'X' & 'O'", () => {
+      const wrapper = mount(<Game />);
+      const firstBtn = wrapper.find("button.tile").at(0);
+      const secondBtn = wrapper.find("button.tile").at(1);
+      const thirdBtn = wrapper.find("button.tile").at(2);
+      firstBtn.simulate("click");
+      secondBtn.simulate("click");
+      thirdBtn.simulate("click");
+      expect(firstBtn.text()).toBe("X");
+      expect(secondBtn.text()).toBe("O");
+      expect(thirdBtn.text()).toBe("X");
+    });
   });
 });
