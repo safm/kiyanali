d4b7c251a34429b4fadbc349a25d3af755528297
Sarfaraz Merchant <smerchant1@wsgc.com>
2020-08-05 22:59:12 -0700

    Test clicking on tile

--- src/__tests__/Tile.test.js
+++ src/__tests__/Tile.test.js
@@ -8,5 +8,19 @@ describe("Tile Unit Test", () => {
       const wrapper = shallow(<Tile />);
       expect(wrapper.find("button.tile").length).toBe(1);
     });
+
+    it("should display passed value as button text", () => {
+      const wrapper = shallow(<Tile value="x" />);
+      expect(wrapper.find("button.tile").text()).toBe("x");
+    });
+  });
+
+  describe("when clicked", () => {
+    it("should call the passed called back with passed position prop", () => {
+      const clickSpy = jest.fn();
+      const wrapper = shallow(<Tile value="x" onClick={clickSpy} position="2" />);
+      wrapper.find("button.tile").simulate("click");
+      expect(clickSpy).toHaveBeenCalledWith("2");
+    });
   });
 });
