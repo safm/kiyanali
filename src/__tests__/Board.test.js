37e8531f512b68e9dcf9aa08dc32d0e15fa9a98a
Sarfaraz Merchant <smerchant1@wsgc.com>
2020-08-05 22:39:44 -0700

    Should render Tile component

--- src/__tests__/Board.test.js
+++ src/__tests__/Board.test.js
@@ -1,12 +1,20 @@
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import React from "react";
 import Board from "../Board";
 
 describe("Board Unit Test", () => {
   describe("when rendered", () => {
     it("should add div with id 'board'", () => {
-      const wrapper = shallow(<Board />);
+      const wrapper = shallow(<Board tiles={[]} />);
       expect(wrapper.find("div#board")).toBeDefined();
+    });
+  });
+
+  describe("when mounted", () => {
+    it("rendered tiles based on props", () => {
+      const tiles = ["x", "o"];
+      const wrapper = mount(<Board tiles={tiles} />);
+      expect(wrapper.find("button.tile").length).toBe(2);
     });
   });
 });
