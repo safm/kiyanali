6de7356119629269d9439fd29496eadb78fc0bc7
Sarfaraz Merchant <smerchant1@wsgc.com>
2020-08-05 22:44:57 -0700

    Game component should load Board component

--- src/__tests__/Game.test.js
+++ src/__tests__/Game.test.js
@@ -1,4 +1,4 @@
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import React from "react";
 import Game from "../Game";
 
@@ -6,7 +6,14 @@ describe("Game Unit Test", () => {
   describe("when rendered", () => {
     it("should show 'Hello World'", () => {
       const wrapper = shallow(<Game />);
-      expect(wrapper.find("div").text()).toBe("Hello World");
+      expect(wrapper.find("div#game").length).toBe(1);
+    });
+  });
+
+  describe("when mounted", () => {
+    it("should render Board component", () => {
+      const wrapper = mount(<Game />);
+      expect(wrapper.find("div#board").length).toBe(1);
     });
   });
 });
