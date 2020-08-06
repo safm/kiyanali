7e58f2d6e188df86b37210ee02921d44278c19bd
Sarfaraz Merchant <smerchant1@wsgc.com>
2020-08-05 22:40:07 -0700

    Rendering Tile component

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Tile from "./Tile";
 
-const Board = () => {
-  return <div id="board"></div>;
+const Board = (props) => {
+  const { tiles } = props;
+  return (
+    <div id="board">
+      {tiles.map((value, index) => (
+        <Tile key={`tile-${index}`} />
+      ))}
+    </div>
+  );
 };
 
 export default Board;
